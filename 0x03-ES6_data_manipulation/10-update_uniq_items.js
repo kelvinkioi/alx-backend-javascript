2262166c11011d5691714afca3d451d5c6d788f1
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-04-15 06:33:01 -0700

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+export default function updateUniqueItems(uniqueItems)
+{
+    if (!(uniqueItems instanceof Map))
+    {
+      throw new Error('Cannot process');
+    }
+    for (const [key, value] of uniqueItems)
+    {
+      if (value === 1) {
+        uniqueItems.set(key, 100);
+      }
+    }
+    return uniqueItems;
+}
