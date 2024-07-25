ca4a2e654f861c0552064e271201850d262416cd
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-07-25 20:44:30 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi - uses the calculateNumber method of Utils', () => {
+    const utilsSpy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utilsSpy.calculateNumber.callCount).to.be.equal(1);
+    utilsSpy.calculateNumber.restore();
+  });
+});
